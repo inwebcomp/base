b71885f1df6d39ff2e3d2e006af7365ae37c7a6b
Alexander Topalo <saneock19@gmail.com>
2020-02-25 20:08:15 +0200

    TranslatableSlug query optimization

--- src/App/Traits/TranslatableSlug.php
+++ src/App/Traits/TranslatableSlug.php
@@ -8,29 +8,26 @@ trait TranslatableSlug
 {
     /**
      * @param Builder $query
-     * @param $slug
+     * @param         $slug
      * @return Builder
      */
     public function scopeFindBySlug(Builder $query, $slug)
     {
         $locale = \App::getLocale();
 
-        $model = (clone $query)->whereHas('translations', function (Builder $query) use ($slug, $locale) {
-            $query->where($this->getTranslationsTable() . '.slug', $slug);
-        })->first();
+        $translationsTable = app()->make($this->getTranslationModelName())->getTable();
+
+        $model = (clone $query)->leftJoin($translationsTable, $this->getTable() . '.' . $this->getKeyName(), '=', $translationsTable . '.' . $this->getForeignKey())->where($this->getTranslationsTable() . '.slug', $slug)->first();
 
         if (! $model)
             return $query->whereKey('-1');
 
         if ($model->hasTranslation($locale)) {
-            $query->whereHas('translations', function (Builder $query) use ($slug, $locale) {
-                $query->where($this->getTranslationsTable() . '.slug', $slug)
-                      ->where($this->getTranslationsTable() . '.' . $this->getLocaleKey(), $locale);
-            });
+            $query->leftJoin($translationsTable, $this->getTable() . '.' . $this->getKeyName(), '=', $translationsTable . '.' . $this->getForeignKey())
+                ->where($this->getTranslationsTable() . '.slug', $slug)
+                ->where($this->getTranslationsTable() . '.' . $this->getLocaleKey(), $locale);
         } else {
-            $query->whereHas('translations', function (Builder $query) use ($slug, $locale) {
-                $query->where($this->getTranslationsTable() . '.slug', $slug);
-            });
+            $query->leftJoin($translationsTable, $this->getTable() . '.' . $this->getKeyName(), '=', $translationsTable . '.' . $this->getForeignKey())->where($this->getTranslationsTable() . '.slug', $slug)->first();
         }
 
         return $query;
