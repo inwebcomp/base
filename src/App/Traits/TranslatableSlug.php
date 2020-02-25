5b946a70adf46f09794f0fbede6811e682251eb8
Alexander Topalo <saneock19@gmail.com>
2020-02-25 20:59:15 +0200

    TranslatableSlug query optimization

--- src/App/Traits/TranslatableSlug.php
+++ src/App/Traits/TranslatableSlug.php
@@ -11,25 +11,51 @@ trait TranslatableSlug
      * @param         $slug
      * @return Builder
      */
+
     public function scopeFindBySlug(Builder $query, $slug)
     {
         $locale = \App::getLocale();
 
-        $translationsTable = app()->make($this->getTranslationModelName())->getTable();
-
-        $model = (clone $query)->leftJoin($translationsTable, $this->getTable() . '.' . $this->getKeyName(), '=', $translationsTable . '.' . $this->getForeignKey())->where($this->getTranslationsTable() . '.slug', $slug)->first();
+        $model = (clone $query)->whereHas('translations', function (Builder $query) use ($slug, $locale) {
+            $query->where($this->getTranslationsTable() . '.slug', $slug);
+        })->first();
 
         if (! $model)
             return $query->whereKey('-1');
 
         if ($model->hasTranslation($locale)) {
-            $query->leftJoin($translationsTable, $this->getTable() . '.' . $this->getKeyName(), '=', $translationsTable . '.' . $this->getForeignKey())
-                ->where($this->getTranslationsTable() . '.slug', $slug)
-                ->where($this->getTranslationsTable() . '.' . $this->getLocaleKey(), $locale);
+            $query->whereHas('translations', function (Builder $query) use ($slug, $locale) {
+                $query->where($this->getTranslationsTable() . '.slug', $slug)
+                    ->where($this->getTranslationsTable() . '.' . $this->getLocaleKey(), $locale);
+            });
         } else {
-            $query->leftJoin($translationsTable, $this->getTable() . '.' . $this->getKeyName(), '=', $translationsTable . '.' . $this->getForeignKey())->where($this->getTranslationsTable() . '.slug', $slug)->first();
+            $query->whereHas('translations', function (Builder $query) use ($slug, $locale) {
+                $query->where($this->getTranslationsTable() . '.slug', $slug);
+            });
         }
 
         return $query;
     }
+    
+//    public function scopeFindBySlug(Builder $query, $slug)
+//    {
+//        $locale = \App::getLocale();
+//
+//        $translationsTable = app()->make($this->getTranslationModelName())->getTable();
+//
+//        $model = (clone $query)->leftJoin($translationsTable, $this->getTable() . '.' . $this->getKeyName(), '=', $translationsTable . '.' . $this->getForeignKey())->where($this->getTranslationsTable() . '.slug', $slug)->first();
+//
+//        if (! $model)
+//            return $query->whereKey('-1');
+//
+//        if ($model->hasTranslation($locale)) {
+//            $query->leftJoin($translationsTable, $this->getTable() . '.' . $this->getKeyName(), '=', $translationsTable . '.' . $this->getForeignKey())
+//                ->where($this->getTranslationsTable() . '.slug', $slug)
+//                ->where($this->getTranslationsTable() . '.' . $this->getLocaleKey(), $locale);
+//        } else {
+//            $query->leftJoin($translationsTable, $this->getTable() . '.' . $this->getKeyName(), '=', $translationsTable . '.' . $this->getForeignKey())->where($this->getTranslationsTable() . '.slug', $slug)->first();
+//        }
+//
+//        return $query;
+//    }
 }
