0f5301e18d3838d0441578d3ea4c47cc223bb21f
Escral <inwebcomp@gmail.com>
2020-09-10 17:18:33 +0300

    Datetime serialization

--- src/App/Entity.php
+++ src/App/Entity.php
@@ -16,6 +16,17 @@ class Entity extends Model
     const STATUS_PUBLISHED = 1;
 
     /**
+     * Prepare a date for array / JSON serialization.
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(\DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+
+    /**
      * Determine if entity is translatable
      *
      * @return bool
